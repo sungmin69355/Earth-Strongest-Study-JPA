82b796e940e171fed5bbb21eeaee49e87c793a1c
sungmin69355 <sungmin69355@naver.com>
2022-07-03 14:01:45 +0900

    DDL 생성기능 테스트

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,17 +1,19 @@
 package hellojpa;
 
 import lombok.Getter;
-import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Getter
-@Setter
+@Table(name = "MBR")
 public class Member {
-
     @Id
     private Long id;
+
+    @Column(name = "USERNAME", unique = true, length = 10) //DDL 제약조건은 DDL을 자동 생성할 때만 사용, JPA 실행 로직에는 영향 x
     private String name;
 }
