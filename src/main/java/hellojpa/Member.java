a0ea2c1d3309c15c7e5edafb40fc810ccdbd3e70
sungmin69355 <sungmin69355@naver.com>
2022-07-09 18:05:43 +0900

    기본키에 시퀀스 적용

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,12 +6,22 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.Date;
 
+/**
+ * 1. 회원은 일반회원과 관리자로 구분해야한다.
+ * 2. 회원가입일과 수정일이 있어야한다.
+ * 3. 회원을 설명할 수 있는 필드가 있어야한다. 이 필드는 길이 제한이 없다.
+ */
 @Entity
 @Getter
 @Setter
 @Table(name = "MBR")
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
-    @Id
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name")
