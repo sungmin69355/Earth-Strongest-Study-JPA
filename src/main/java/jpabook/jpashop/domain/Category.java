34fd9b4ecb3fc36bf98ab2a9f733a05372a90930
sungmin69355 <sungmin69355@naver.com>
2022-08-14 18:16:13 +0900

    배송, 카테고리 기능 Entity 설계

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    /**
+     * 상속카테고리 ex) 전자재품 -> 핸드폰
+     */
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name= "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "TTEM_ID")
+    )
+    private List<Item> items = new ArrayList<>();
+}
